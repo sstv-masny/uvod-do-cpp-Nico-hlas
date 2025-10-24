3286d9ec496c27de1aca57df7c126bd70c1d40e7
Nico-hlas <turacnicolas@gmail.com>
2025-10-24 08:35:55 +0200

    Update main.cpp

--- task_01_even_or_odd/main.cpp
+++ task_01_even_or_odd/main.cpp
@@ -4,7 +4,8 @@ int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
     long long n;
-    if (!(std::cin >> n)) return 0;
-    // TODO: print "even" or "odd"
-    return 0;
-}
+    std::cout << "Nacital cislo: ";
+    std::cin >> n;
+    if (!(n)) {
+        return 0;
+    }
