122b70a8bc2acf2bd65c412454cacf4315412658
JurajVojtas <krivanikmatus@gmail.com>
2025-11-07 08:35:21 +0100

    in

--- task_02_min_of_three/main.cpp
+++ task_02_min_of_three/main.cpp
@@ -1,10 +1,11 @@
 
 #include <iostream>
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    long long a,b,c;
-    if (!(std::cin >> a >> b >> c)) return 0;
+
+    int a, b, c;
+    if (a << b << c) {
+        }
+
     // TODO: print the minimum of a, b, c
     return 0;
 }
--- task_03_taxi_fare/main.cpp
+++ task_03_taxi_fare/main.cpp
@@ -3,10 +3,7 @@
 #include <iomanip>
 #include <cmath>
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    long long k;
-    if (!(std::cin >> k)) return 0;
+
     // TODO: compute the fare as specified and print with two decimals
     return 0;
 }
